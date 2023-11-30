66181bd06da9daf4d35a37c810f3ae176e6c2495
joaofaria <joao.pedrosafaria@gmail.com>
2023-11-29 21:18:47 -0300

    Teste de permissões

--- estoque/views.py
+++ estoque/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Produto, Categoria
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 @login_required
 def index(request):
@@ -14,14 +14,18 @@ def teste(request):
     return render(request, 'estoque/teste.html')
 
 ##### Cadastrar #####
-class CategoriaCreate(LoginRequiredMixin, CreateView):
+class CategoriaCreate(PermissionRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Categoria
+    permission_required = "estoque.add_categoria"
     fields = ['nome_categoria']
     template_name = 'estoque/form.html'
     success_url = reverse_lazy('produtos')
 
-class ProdutoCreate(LoginRequiredMixin, CreateView):
+class ProdutoCreate(PermissionRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')   
     model = Produto
+    permission_required = "estoque.add_produto"
     fields = ['nome_produto', 'categoria', 'quantidade_estoque', 'valor_custo', 'valor_venda', 'disponivel']
     template_name = 'estoque/form.html'
     success_url = reverse_lazy('produtos')
@@ -29,12 +33,14 @@ class ProdutoCreate(LoginRequiredMixin, CreateView):
 ##### Editar #####
 
 class CategoriaUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Categoria
     fields = ['nome_categoria']
     template_name = 'estoque/form.html'
     success_url = reverse_lazy('produtos')
 
 class ProdutoUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Produto
     fields = ['nome_produto', 'categoria', 'quantidade_estoque', 'valor_custo', 'valor_venda', 'disponivel']
     template_name = 'estoque/form.html'
@@ -42,17 +48,20 @@ class ProdutoUpdate(LoginRequiredMixin, UpdateView):
 
 ##### Excluir #####
 class CategoriaDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Categoria
     template_name = 'estoque/form-excluir.html'
     success_url = reverse_lazy('produtos')
 
 class ProdutoDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Produto
     template_name = 'estoque/form-excluir.html'
     success_url = reverse_lazy('produtos')
 
 ##### Visualizar #####
 class ProdutoList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Produto
     template_name = 'estoque/produtos.html'
     success_url = reverse_lazy('produtos')
